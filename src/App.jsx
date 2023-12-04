56934018189f353697f6f29e77342a8668675b02
imran <alimrancv870@gmail.com>
2023-12-05 00:16:44 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { React } from 'react'
-import Header from './components/header';
-import Banner from './components/banner';
+import Header from './components/Header';
+import Banner from './components/Banner';
 import Footer from './components/Footer';
 
 function App() {
